ec79f2bb58939ab99ec828fb70d53f567f9e37ed
Chalouw137 <chalouwgrossouw@gmail.com>
2025-04-14 01:33:41 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, debug=False)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT if available
+    socketio.run(app, host='0.0.0.0', port=port)
